132c77605b9fccc7ae1aea6c18954b8cef92f4cc
student <111gagara111@gmail.com>
2024-09-18 22:27:18 +0300

    16.09.24 потуги на шашки2

---
+++ src/Checkers.js
@@ -0,0 +1,10 @@
+import EnemyInfo from "./EnemyInfo";
+import PlayerInfo from "./PlayerInfo";
+import Game from "./Game";
+
+function Checkers(){
+    return [<EnemyInfo/>,<Game/>,<PlayerInfo/>
+    ];
+}
+
+export default Checkers;
---
+++ src/PlayerInfo.js
@@ -0,0 +1,5 @@
+function PlayerInfo() {
+    return <div id="PlayerInfo"></div>;
+}
+
+export default  PlayerInfo;
