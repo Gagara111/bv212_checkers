116b801fd98fb188a40f2ad623c85b079360f252
student <111gagara111@gmail.com>
2024-10-09 20:25:56 +0300

    шашаки навсегда 10.10.24

--- src/DOM/Checkers/Game/Board/BlackChecker.js
+++ src/DOM/Checkers/Game/Board/BlackChecker.js
@@ -4,6 +4,6 @@ function BlackChecker(props) {
     return <div style={{groundColumn:props.y, gridRow:props.x}}>
            <img src={Image} alt={""}/>
            </div>;
-      }
+}
 
 export default BlackChecker;
